0442f3b00eaeafb0e4e73ca36d1414acf6d9c3cc
Obaa10 <obada.maleh@gmail.com>
2023-09-20 09:50:32 +0200

    update index

--- user-service/index.ts
+++ user-service/index.ts
@@ -1,9 +1,14 @@
 // Import the express in typescript file
-import express from 'express';
- 
+import express, { Router } from 'express';
+import Routes from './src/routes';
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Initialize the express engine
 const app: express.Application = express();
- 
+
 // Take a port 3000 for running server.
 const port: number = 3000;
 
@@ -11,9 +16,11 @@ const port: number = 3000;
 app.get('/', (_req, _res) => {
     _res.send("TypeScript With Express");
 });
- 
+
 // Server setup
 app.listen(port, () => {
     console.log(`TypeScript with Express
          http://localhost:${port}/`);
 });
+
+new Routes(app);
